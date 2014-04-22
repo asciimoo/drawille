32440250f2d7404b3294bcfb9d90bcb63ca2c46b
Adam Tauber <asciimoo@gmail.com>
2014-04-22 16:58:12 +0200

    [enh] examples added

---
+++ examples/sine_tracking.py
@@ -0,0 +1,16 @@
+from drawille import Canvas, line
+import math
+from time import sleep
+c = Canvas()
+i = 0
+height = 40
+while True:
+    for x,y in line(0, height, 180, int(math.sin(math.radians(i))*height + height)):
+        c.set(x,y)
+    for x in range(0, 360, 2):
+        coords = (x/2, height + int(round(math.sin(math.radians(x+i)) * height)))
+        c.set(*coords)
+    print "\x1b[2J\x1b[H" + c.frame()
+    i += 2
+    sleep(1.0/24)
+    c.clear()
