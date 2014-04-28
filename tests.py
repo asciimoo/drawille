c063ceb3aeba072a1238231692dd891f5d6a2024
Adam Tauber <asciimoo@gmail.com>
2014-04-28 18:48:47 +0200

    [enh] moar tests

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from drawille import Canvas
+from drawille import Canvas, line
 from unittest import TestCase, main
 
 
@@ -12,10 +12,33 @@ class CanvasTestCase(TestCase):
         self.assertTrue(1 in c.pixels and 1 in c.pixels[1])
 
 
-    def test_unset(self):
+    def test_unset_empty(self):
         c = Canvas()
         c.set(1,1)
         c.unset(1, 1)
+        self.assertEqual(c.pixels, dict())
+
+
+    def test_unset_nonempty(self):
+        c = Canvas()
+        c.set(1,1)
+        c.set(2,1)
+        c.unset(1, 1)
+        self.assertEqual(c.pixels, {1:{2: 2}})
+
+
+    def test_clear(self):
+        c = Canvas()
+        c.set(1,1)
+        c.clear()
+        self.assertEqual(c.pixels, dict())
+
+
+    def test_toggle(self):
+        c = Canvas()
+        c.toggle(0,0)
+        self.assertEqual(c.pixels, {0: {0: 1}})
+        c.toggle(0,0)
         self.assertEqual(c.pixels, dict())
 
 
@@ -24,6 +47,25 @@ class CanvasTestCase(TestCase):
         c.set(0,0)
         self.assertEqual(c.frame(), '\xe2\xa0\x81')
         self.assertEqual(c.frame(), '⠁')
+
+
+class LineTestCase(TestCase):
+
+
+    def test_single_pixel(self):
+        self.assertEqual(list(line(0, 0, 0, 0)), [(0, 0)])
+
+
+    def test_row(self):
+        self.assertEqual(list(line(0, 0, 1, 0)), [(0, 0), (1, 0)])
+
+
+    def test_column(self):
+        self.assertEqual(list(line(0, 0, 0, 1)), [(0, 0), (0, 1)])
+
+
+    def test_diagonal(self):
+        self.assertEqual(list(line(0, 0, 1, 1)), [(0, 0), (1, 1)])
 
 
 if __name__ == '__main__':
