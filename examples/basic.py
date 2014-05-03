491b1fdaae3c8586f56f9c7a1a42bcab485d574d
Adam Tauber <asciimoo@gmail.com>
2014-05-04 01:36:36 +0200

    [fix] parametering

--- examples/basic.py
+++ examples/basic.py
@@ -6,29 +6,29 @@ import math
 s = Canvas()
 
 for x in range(1800):
-    s.set((x/10, math.sin(math.radians(x)) * 10))
+    s.set(x/10, math.sin(math.radians(x)) * 10)
 
 print(s.frame())
 
 s.clear()
 
 for x in range(0, 1800, 10):
-    s.set((x/10, 10 + math.sin(math.radians(x)) * 10))
-    s.set((x/10, 10 + math.cos(math.radians(x)) * 10))
+    s.set(x/10, 10 + math.sin(math.radians(x)) * 10)
+    s.set(x/10, 10 + math.cos(math.radians(x)) * 10)
 
 print(s.frame())
 
 s.clear()
 
 for x in range(0, 3600, 20):
-    s.set((x/20, 4 + math.sin(math.radians(x)) * 4))
+    s.set(x/20, 4 + math.sin(math.radians(x)) * 4)
 
 print(s.frame())
 
 s.clear()
 
 for x in range(0, 360, 4):
-    s.set((x/4, 30 + math.sin(math.radians(x)) * 30))
+    s.set(x/4, 30 + math.sin(math.radians(x)) * 30)
 
 for x in range(30):
     for y in range(30):
