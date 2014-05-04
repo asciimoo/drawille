6b6ff6e6a8c56738fb7902916fdfd78372dc5b8f
Adam Tauber <asciimoo@gmail.com>
2014-05-04 02:27:46 +0200

    [enh] set_text test

--- tests.py
+++ tests.py
@@ -8,43 +8,49 @@ class CanvasTestCase(TestCase):
 
     def test_set(self):
         c = Canvas()
-        c.set(1,1)
+        c.set(1, 1)
         self.assertTrue(1 in c.pixels and 1 in c.pixels[1])
 
 
     def test_unset_empty(self):
         c = Canvas()
-        c.set(1,1)
+        c.set(1, 1)
         c.unset(1, 1)
         self.assertEqual(c.pixels, dict())
 
 
     def test_unset_nonempty(self):
         c = Canvas()
-        c.set(1,1)
-        c.set(2,1)
+        c.set(1, 1)
+        c.set(2, 1)
         c.unset(1, 1)
         self.assertEqual(c.pixels, {1:{2: 2}})
 
 
     def test_clear(self):
         c = Canvas()
-        c.set(1,1)
+        c.set(1, 1)
         c.clear()
         self.assertEqual(c.pixels, dict())
 
 
     def test_toggle(self):
         c = Canvas()
-        c.toggle(0,0)
+        c.toggle(0, 0)
         self.assertEqual(c.pixels, {0: {0: 1}})
-        c.toggle(0,0)
+        c.toggle(0, 0)
         self.assertEqual(c.pixels, dict())
+
+
+    def test_set_text(self):
+        c = Canvas()
+        c.set_text(0, 0, "asdf")
+        self.assertEqual(c.frame(), "asdf")
 
 
     def test_frame(self):
         c = Canvas()
-        c.set(0,0)
+        c.set(0, 0)
         self.assertEqual(c.frame(), '⠁')
 
 
