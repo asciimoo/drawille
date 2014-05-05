00bb500633c7cb79a4251f1006f17d364724d8b0
Adam Tauber <asciimoo@gmail.com>
2014-05-05 13:15:16 +0200

    [enh] getTerminalSize added

--- drawille.py
+++ drawille.py
@@ -24,6 +24,37 @@ IS_PY3 = version_info[0] == 3
 if IS_PY3:
     from functools import reduce
 
+
+# http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python
+def getTerminalSize():
+    import os
+    env = os.environ
+
+    def ioctl_GWINSZ(fd):
+        try:
+            import fcntl, termios, struct
+            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
+        '1234'))
+        except:
+            return
+        return cr
+
+    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+
+    if not cr:
+        try:
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            cr = ioctl_GWINSZ(fd)
+            os.close(fd)
+        except:
+            pass
+
+    if not cr:
+        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
+
+    return int(cr[1]), int(cr[0])
+
+
 """
 
 source: http://www.alanwood.net/unicode/braille_patterns.html
