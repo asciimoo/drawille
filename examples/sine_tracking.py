98957c8809c45fa49f3afea6d7efd87f1fcb80ef
Adam Tauber <asciimoo@gmail.com>
2014-06-28 02:00:46 +0200

    [enh] example update - using animate()

--- examples/sine_tracking.py
+++ examples/sine_tracking.py
@@ -1,37 +1,26 @@
 from __future__ import print_function
-from drawille import Canvas, line
+from drawille import Canvas, line, animate
 import math
-from time import sleep
-import curses
-import locale
 
-locale.setlocale(locale.LC_ALL,"")
-
-stdscr = curses.initscr()
-stdscr.refresh()
-
-
-def __main__(stdscr):
+def __main__():
     i = 0
-    c = Canvas()
     height = 40
-    while True:
 
-        for x,y in line(0, height, 180, int(math.sin(math.radians(i)) * height + height)):
-            c.set(x,y)
+    while True:
+        frame = []
+        for x,y in line(0, height, 180, math.sin(math.radians(i)) * height + height):
+            frame.append((x,y))
 
         for x in range(0, 360, 2):
-            coords = (x/2, height + int(round(math.sin(math.radians(x+i)) * height)))
-            c.set(*coords)
+            coords = (x/2, height + math.sin(math.radians(x+i)) * height)
+            frame.append((coords[0], coords[1]))
 
-        f = c.frame()
-        stdscr.addstr(0, 0, '{0}\n'.format(f))
-        stdscr.refresh()
+        yield frame
 
         i += 2
-        sleep(1.0/24)
-        c.clear()
+
 
 
 if __name__ == '__main__':
-    curses.wrapper(__main__)
+    c = Canvas()
+    animate(c, __main__, 1./60)
