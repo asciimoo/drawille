72f06ba95e554b1730d2f4ac628f75619e3d2eef
Adam Tauber <asciimoo@gmail.com>
2016-04-03 00:00:01 +0200

    [enh] version bump to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'drawille',
-    version = '0.0.4',
+    version = '0.1.0',
     author = 'Adam Tauber',
     author_email = 'asciimoo@gmail.com',
     description = ('Drawing in terminal with unicode braille characters'),
