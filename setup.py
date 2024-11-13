0d8dbea4c6ed698bc1443c5cc7288cbdf884f8e5
Adam Tauber <asciimoo@gmail.com>
2024-11-13 14:37:02 +0100

    [enh] release v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'drawille',
-    version = '0.1.0',
+    version = '0.2.0',
     author = 'Adam Tauber',
     author_email = 'asciimoo@gmail.com',
     description = ('Drawing in terminal with unicode braille characters'),
